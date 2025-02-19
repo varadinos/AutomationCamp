3a01bbd19381fdcb17e6bc20639a0fb5d3f18b03
stoyanvaradinov <varadinovs@gmail.com>
2025-02-20 00:57:14 +0200

    changed frame locators

--- pages/FrameInteractionsPage.ts
+++ pages/FrameInteractionsPage.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page } from '@playwright/test';
+import { FrameLocator, type Locator, type Page } from '@playwright/test';
 
 export class FrameInteractionsPage {
     readonly page: Page;
@@ -6,16 +6,23 @@ export class FrameInteractionsPage {
     readonly frameOneButton: Locator;
     readonly frameTwoButton: Locator;
     readonly frameFourButton: Locator;
+    readonly frameOne: FrameLocator;
+    readonly frameTwo: FrameLocator;
+    readonly frameThree: FrameLocator;
+    readonly frameFour: FrameLocator;
 
 
 
     constructor (page: Page){
         this.page = page;
         this.url = 'https://play1.automationcamp.ir/frames.html';
-        this.frameOneButton = page.frameLocator('#frame1').locator('#click_me_1');
-        this.frameTwoButton = page.frameLocator('#frame1').frameLocator('#frame2').locator('#click_me_2');
-        this.frameFourButton = page.frameLocator('#frame1').frameLocator('#frame3')
-        .frameLocator('#frame4').locator('#click_me_4');
+        this.frameOne = page.frameLocator('#frame1');
+        this.frameTwo = this.frameOne.frameLocator('#frame2')
+        this.frameThree = this.frameOne.frameLocator('#frame3')
+        this.frameFour = this.frameThree.frameLocator('#frame4');
+        this.frameOneButton = this.frameOne.locator('#click_me_1');
+        this.frameTwoButton = this.frameTwo.locator('#click_me_2');
+        this.frameFourButton = this.frameFour.locator('#click_me_4');
     }
 
     async goTo():Promise<void> {
