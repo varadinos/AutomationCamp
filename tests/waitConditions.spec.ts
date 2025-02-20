28de3984f5641f18e05eafadb8f33c95dc764b65
stoyanvaradinov <varadinovs@gmail.com>
2025-02-20 22:55:25 +0200

    test name change

--- tests/waitConditions.spec.ts
+++ tests/waitConditions.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "../setup/fixtures";
 
-test('Wait for element to be visible', async ({waitConditionsPage}) => {
+test('Trigger visibility and verify element appears with correct text', async ({waitConditionsPage}) => {
     await waitConditionsPage.clickVisibilityTriggerButton();
     await waitConditionsPage.clickVisibilityTargetButton();
     await expect(waitConditionsPage.visibilityPopoverHeader).toBeVisible();
